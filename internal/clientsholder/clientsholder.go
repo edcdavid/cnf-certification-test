6d4d8ecb19fbf1d3aea70414ea2ced1f4040d594
David Elie-Dit-Cosaque <deliedit@redhat.com>
2022-03-02 14:21:52 -0600

    proof of concept for getting openshift version

--- internal/clientsholder/clientsholder.go
+++ internal/clientsholder/clientsholder.go
@@ -17,18 +17,25 @@
 package clientsholder
 
 import (
+	"context"
+	"errors"
 	"time"
 
+	configv1 "github.com/openshift/api/config/v1"
 	clientconfigv1 "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
+	configv1client "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
 	clientOlm "github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned"
 	"github.com/sirupsen/logrus"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	appv1client "k8s.io/client-go/kubernetes/typed/apps/v1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 )
 
 type ClientsHolder struct {
@@ -40,6 +47,7 @@ type ClientsHolder struct {
 	OlmClient     *clientOlm.Clientset
 	AppsClients   *appv1client.AppsV1Client
 	K8sClient     *kubernetes.Clientset
+	oClient			*configv1client.ConfigV1Client
 
 	ready bool
 }
@@ -104,6 +112,41 @@ func NewClientsHolder(filenames ...string) *ClientsHolder { //nolint:funlen // t
 	if err != nil {
 		logrus.Panic("can't instantiate k8sclient", err)
 	}
+	// create the oc client
+	clientsHolder.oClient, err = configv1client.NewForConfig(clientsHolder.RestConfig)
+	if err != nil {
+		logrus.Panic("can't instantiate ocClient", err)
+	}
+
+	openshiftVersion,_:=getOpenshiftVersion()
+	k8sVersion, err:=clientsHolder.K8sClient.DiscoveryClient.ServerVersion()
+	logrus.Infof("k8sVersion=%s openshiftVersion=%s",k8sVersion,openshiftVersion )
+
 	clientsHolder.ready = true
 	return &clientsHolder
 }
+
+
+
+func getOpenshiftVersion()(ver string, err error){
+	var clusterOperator *configv1.ClusterOperator
+			clusterOperator, err = clientsHolder.oClient.ClusterOperators().Get(context.TODO(), "openshift-apiserver", metav1.GetOptions{})
+			// error here indicates logged in as non-admin, log and move on
+			if err != nil {
+				switch {
+				case kerrors.IsForbidden(err), kerrors.IsNotFound(err):
+					klog.V(5).Infof("OpenShift Version not found (must be logged in to cluster as admin): %v", err)
+					err = nil
+				}
+			}
+			if clusterOperator != nil {
+				for _, ver := range clusterOperator.Status.Versions {
+					if ver.Name == "operator" {
+						// openshift-apiserver does not report version,
+						// clusteroperator/openshift-apiserver does, and only version number
+						return ver.Version, nil
+					}
+				}
+			} 
+			return "", errors.New("could not get openshift version")
+		}
